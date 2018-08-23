a84bf78f95410b870aad920d151dee37c862d101
sinytim <sinytim@mail.ru>
2018-08-24 01:26:01 +0300

    .

--- mainApp/static/mainApp/scripts.js
+++ mainApp/static/mainApp/scripts.js
@@ -7,16 +7,3 @@ function expandMenu() {
     x.className = "";
   }
 }
-
-// window.onscroll = function() { myFunction() };
-//
-// var navbar = document.getElementById("top-nav");
-// var sticky = navbar.offsetTop;
-//
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
